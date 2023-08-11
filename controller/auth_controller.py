fc95154b694fc96eb87effb5bcf97b27dc97eecf
Chérif BELLAHOUEL <cherifb@mbp-de-cherif.home>
2023-08-11 11:45:30 +0200

    add hash password

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -3,6 +3,15 @@ from fastapi import Depends
 from infra.db import get_session
 from repository.user_repository import UserRepository
 from model.user_model import User
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 class AuthController():
 
@@ -11,15 +20,16 @@ class AuthController():
         self.user_repo = UserRepository(db)
     
     async def create_user(self, new_user: User.Model) -> User.Model:
+        new_user.password = get_password_hash(new_user.password)
         await self.user_repo.add_user(new_user)
         return new_user
     
     async def login_user(self, email, password):
         user = await self.user_repo.get_user_by_email(email)
-        if(user == None):
-            return "une erreur, l'utilisateur n'existe pas"
-        if(user.password != password):
-            return "une erreur, les mots de passe ne correspondent pas"
+        if not user:
+            return False
+        if not verify_password(password, user.password):
+            return False
         return user
     
 
